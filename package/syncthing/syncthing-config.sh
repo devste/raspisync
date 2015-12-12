4f03937c257e82a8e792bd742dd6be0dc9ea8155
devste <stephen+github@mailbox.org>
2015-12-12 10:30:23 +0100

    Opening up web interface to network

--- package/syncthing/syncthing-config.sh
+++ package/syncthing/syncthing-config.sh
@@ -17,8 +17,8 @@ fi
 # Change directory due to all the backup files created
 cd ${ST_CONF_DIR}
 
-echo "Disabling GUI"
-sed -i.gui.bak -e 's/<gui enabled="true"/<gui enabled="false"/' ${ST_CONF} 
+echo "Enforcing TLS on GUI"
+sed -i.gui-tls.bak -e 's/<gui enabled=".*" tls=".*"/<gui enabled="true" tls="true"/' ${ST_CONF} 
 
 echo "Disabling automatic updates"
 sed -i.autoupdate.bak -r 's/<autoUpgradeIntervalH>.+?<\/autoUpgradeIntervalH>/<autoUpgradeIntervalH>0<\/autoUpgradeIntervalH>/' ${ST_CONF}
@@ -28,3 +28,13 @@ sed -i.upnp.bak -r 's/<upnpEnabled>true<\/upnpEnabled>/<upnpEnabled>false<\/upnp
 
 echo "Disabling browser start"
 sed -i.browser.bak -r 's/<startBrowser>true<\/startBrowser>/<startBrowser>false<\/startBrowser>/' ${ST_CONF}
+
+echo "Allowing GUI access from the network"
+# Backup file first
+cp ${ST_CONF} ${ST_CONF}.gui-open.bak
+# Transform to single-line file
+cat ${ST_CONF} | tr '\n' '\r' > ${ST_CONF}.tmp.gui-open
+# Replace IP address
+sed -i.bak -r 's;(.*)<gui(.*)>(.*)<address>(.*):(.*)</address>(.*)</gui>(.*);\1<gui\2>\3<address>0.0.0.0:\5</address>\6</gui>\7;' ${ST_CONF}.tmp.gui-open
+# Write the new config file back
+cat ${ST_CONF}.tmp.gui-open | tr '\r' '\n' > ${ST_CONF}
